85bf5f1219cdcab799fd0cbd842824b07a7c92c1
joeappleton18 <joe.appleton@solent.ac.uk>
2020-10-10 06:57:31 +0100

    inital commit

---
+++ src/Components/Week1/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Week1 = () => {
+  const name = "Joe";
+
+  const location = () => "Brighton";
+
+  return <h1>Hello this is week 1</h1>;
+};
+
+export default Week1;
