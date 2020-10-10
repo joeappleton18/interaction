ac410ae2de3161c0176e8a9f0fc655f7fef6b1e2
joeappleton18 <joe.appleton@solent.ac.uk>
2020-10-10 07:09:22 +0100

    Updates

---
+++ precache-manifest.779f965ebef51d5d92bef5bbc2f11aef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2be189a4b8a26f6cf937582477b48946",
+    "url": "/interaction/index.html"
+  },
+  {
+    "revision": "e1ea153c8a97b8539fa5",
+    "url": "/interaction/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a5ba0efd6709bdac6c43",
+    "url": "/interaction/static/js/2.a3f36936.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/interaction/static/js/2.a3f36936.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1ea153c8a97b8539fa5",
+    "url": "/interaction/static/js/main.b7b8e5a2.chunk.js"
+  },
+  {
+    "revision": "b5dd00eeda3cf030f422",
+    "url": "/interaction/static/js/runtime-main.aaeaceaa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/interaction/static/media/logo.5d5d9eef.svg"
+  }
+]);
