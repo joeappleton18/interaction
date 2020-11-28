a3e2be805712318b5ae4e939b1615132a5d6a3f6
joeappleton18 <joe.appleton@solent.ac.uk>
2020-11-28 07:32:26 +0000

    week 9 complete

--- src/Components/Week9/index.js
+++ src/Components/Week9/index.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import styled, { keyframes } from 'styled-components';
 
+const time = '1s';
+const funkyBackground = 'linear-gradient(-45deg, #ee7752, #e73c7e, #23d4ab)';
+const setAnimation = (from, to) => keyframes({ from: from, to: to })
 
 const StyledWrapper = styled.div`
-    background:  ${({ showBackground }) => showBackground && 'linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab)'};
+    animation: ${time} ${({ showBackground }) => showBackground && setAnimation({ background: 'white' }, { background: funkyBackground })} linear;
+    animation-fill-mode: forwards;
     display: flex;
     height: 100vh;
     width: 100vw;
@@ -12,75 +16,59 @@ const StyledWrapper = styled.div`
 `;
 
 const StyledButton = styled.div`
-   
     border-radius: 5rem;
     border: solid 0.5rem;
     background-color: #313130;
     color: #b95951;
     height: 10rem;
     width: 20rem;
-    
 `;
 
-const StyledSwitch = styled.span`
-    box-sizing: border-box;
-    display: block;
-    animation:  1s ${({ active }) => active && keyframes({ from: { marginLeft: 0 }, to: { marginLeft: '180px' } })} linear;
-    animation-fill-mode: forwards;
-    height: 9rem;
-    margin: 0.7rem;
-    width: 8.125rem;
-    background: linear-gradient(180deg, #4b4a49, #313130);
-    top: 0.3125rem;
-    left: 0.3125rem;
-    border: solid 0.3125rem;
+const StyledSwitch = styled.div`
     border-radius: 5rem;
-    
-
+    animation: ${time} ${({ active }) => active && setAnimation({ marginLeft: 0 }, { marginLeft: '180px' })} linear;
+    animation-fill-mode: forwards;
+    border: solid 0.3125rem;
+    height: 9rem;
+    width: 8.125rem;
+    margin: 0.7rem;
+    background: linear-gradient(180deg, #4b4a49, #313130);
+    margin: 0.4rem 0 0.2rem 0.1rem;
+     
 `;
 
-
-/*
-
-import Week8 from "./Components/Week8";
-https://codepen.io/jrsinclair/pen/KqZBWM
-*/
 
 const Toggle = (props) => {
-
-    const { onActive, onNotActive } = props;
     const [active, setActive] = useState(false);
+    const { onActive, onNotActive } = props;
 
     useEffect(() => {
-
         if (!active) {
             onNotActive();
             return;
         }
 
-        onActive()
+        onActive();
 
     }, [active])
 
 
-
     const handleClick = () => setActive(!active);
-    return (<StyledButton> <StyledSwitch active={active} onClick={handleClick}></StyledSwitch> </StyledButton>)
+
+    return (<StyledButton> <StyledSwitch active={active} onClick={handleClick} /> </StyledButton>)
 }
+
 
 const Week9 = () => {
 
-    const [showBackground, setShowBackGround] = useState(false);
+    const [showBackground, setShowBackground] = useState(false);
+    const handleNotActive = () => setShowBackground(false);
+    const handleActive = () => setShowBackground(true);
 
-    const handleActive = () => {
-        setShowBackGround(true);
-    }
-
-    const handleNotActive = () => {
-        setShowBackGround(false);
-    }
-
-    return <><StyledWrapper showBackground={showBackground}> <Toggle onNotActive={handleNotActive} onActive={handleActive} /></StyledWrapper></>
+    return (<StyledWrapper showBackground={showBackground}>
+        <Toggle onNotActive={handleNotActive} onActive={handleActive} />
+        
+    </StyledWrapper>)
 }
 
 export default Week9;
